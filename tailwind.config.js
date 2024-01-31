1363ca7c3f550ab13f0725cd11af2474b0ad792d
ryan <ryanlin.hr@gmail.com>
2024-01-31 23:17:39 +0800

    updating navbar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    theme: {
+        fontFamily: {
+            'sans': ['minsans', 'sans-serif'],
+            'sarif': ['orifont', 'sarif'],
+        },
+    extend: {},
+  },
+  plugins: [],
+  
+}
+}
