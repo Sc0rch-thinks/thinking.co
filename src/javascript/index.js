b8f6d2c3ab8e6ee118868f83c9f7d41cbb278ea6
ryan <ryanlin.hr@gmail.com>
2024-02-02 15:27:42 +0800

    made a overlay

---
+++ src/javascript/index.js
@@ -0,0 +1,11 @@
+document.getElementById("overlay").style.display =
+	"none";
+document
+	.getElementById("menu-icon")
+	.addEventListener("click", function (e) {
+		// Prevent default action of the button
+		e.preventDefault();
+		document.getElementById(
+			"overlay"
+		).style.display = "block";
+	});
